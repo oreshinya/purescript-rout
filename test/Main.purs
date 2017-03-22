48af888570879927d3bd60293b04b89e56aea9c8
shinya takahashi <s.takahashi313@gmail.com>
2017-03-22 22:15:52 +0900

    Added tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,30 @@ module Test.Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Test.Assert (assert, ASSERT)
+import Rout (match, lit, int, param, end)
+import Data.Maybe (fromMaybe)
+import Control.Alt ((<|>))
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+data Route
+  = Home
+  | User Int
+  | Users String
+  | NotFound
+
+derive instance routeEq :: Eq Route
+route :: String -> Route
+route url = fromMaybe NotFound $ match url $
+  Home <$ end
+  <|>
+  Users <$> (lit "users" *> param "name") <* end
+  <|>
+  User <$> (lit "users" *> int) <* end
+
+main :: forall e. Eff (assert :: ASSERT | e) Unit
 main = do
-  log "You should add some tests."
+  assert $ route "" == Home
+  assert $ route "/" == Home
+  assert $ route "/users?name=oreshinya" == Users "oreshinya"
+  assert $ route "/users/42" == User 42
+  assert $ route "/projects" == NotFound
